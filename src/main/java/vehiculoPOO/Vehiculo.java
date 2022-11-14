727e5c0d3543a210d4e5e8b88e3f075c9e6db109
jsalgua027 <jsalcedoguarde@gmail.com>
2022-11-14 13:13:35 +0100

    Mejorado las respuestas teóricas de los comentarios

--- src/main/java/vehiculoPOO/Vehiculo.java
+++ src/main/java/vehiculoPOO/Vehiculo.java
@@ -24,14 +24,32 @@ package vehiculoPOO;
      * 
      *- ¿Qué es un programa orientado a objetos?
      * es la interacion que existe entre nuestros objetos metidos en diferentes clases
+
        -¿Qué es una clase?
       * es un molde donde vamos a indicar los atributos y los métodos de los objetos que queremos modelar
+
        - ¿Qué es un objeto?
         * es una variable(no de tipo primitivo), es de tipo referencia que contoenen los datos concretos de la instancia de una clase
+            objetos : tienen una identidad que es el nombre;
+            estado : los valores de sus atributos;
+            comportamiento: los metodos, (Todo lo que es capar de hacer)
+
         * ¿instancia de una clase?
         ¿Cuáles son los pilares de la POO? Explica brevemente cada uno de ellos.
-        * definicion, 
-        * atributos metodos
+
+        -Abstracción. Mediante la creación de clases, que son los elementos abstractos del programa informático.
+        Encapsulamiento. Permite ocultar la implementación (cómo es su código) de las clases ofreciendo a
+        l programador una serie de funciones para interaccionar con los objetos.
+
+         -Herencia. Todas las clases Java se estructuran formando jerarquías de clases, en forma de árbol. 
+        Estas características permiten la reutilización de código. Por ejemplo, imaginemos una clase Cliente, ya implementada. 
+        Si quisiéramos crear la clase ClienteVip, podríamos utilizar la clase Cliente y añadir sólo lo referente a ClienteVip.
+        Esta forma de crear nuevas clases a partir de otras es la base de la herencia en Java, ya que unas clases heredan características de otras.
+
+        -Polimorfismo. Hace referencia a la posibilidad de enviar mensajes sintácticamente iguales a objetos diferentes. 
+        No se debe confundir con la sobrecarga de métodos.
+
+
         ¿Qué ventajas aporta la POO?
                 Modularidad. El código de una clase se puede mantener y reescribir sin tener que tocar el código de otras clases.
                 Reutilización de código. Podemos hacer uso de clases y objetos diseñados por terceras personas sin saber el código que contienen, haciendo uso de su interfaz.
@@ -39,17 +57,23 @@ package vehiculoPOO;
 
         ¿Cuáles son las partes de una clase?
                 Declaracion y Definición
+
         ¿Qué elementos componen la definición de una clase? Explica brevemente cada uno
                 Todo lo que este entre las llaves egipcias de inicio a fin { }, normalmente están los
                  métodos y los atributos
+
         ¿Qué es un método constructor?
                 Es un método que nos sirve para construir los objetos de la clase
+
         ¿Qué son los métodos de consulta o getters?
             Son aquellos que nos ayudan a consultar los estados del objeto (Atributos)
+
         ¿Qué son los métodos modificadores o setters?
             Son aquellos que nos sivern para modificar los atributos del objeto creado
+
         ¿En qué consiste la instanciación de un objeto? ¿Para qué sirve el operador ‘new’?
             En reservar espacio de memoria para el objeto, para ello se usa el operador new (creas el objeto)
+
         ¿Cuál es la función del método main()? ¿Cómo se declara el método main()?
             Sirve para ejecutar ejecutar un programa Java. la declaracion es: 
                 public static void main(String args[ ]){
