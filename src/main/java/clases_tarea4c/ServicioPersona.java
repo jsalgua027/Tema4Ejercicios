3b79494f3f14eaee142471b1631133140960853e
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2022-11-17 16:50:54 +0100

    Añadido un filtro más a la solicitud de datos para que no meta números negativos

--- src/main/java/clases_tarea4c/ServicioPersona.java
+++ src/main/java/clases_tarea4c/ServicioPersona.java
@@ -12,79 +12,74 @@ import java.util.Scanner;
  * @author Windows10
  */
 public class ServicioPersona {
-    
+
     private static Scanner teclado = new Scanner(System.in);
-    
+
     public static int leerEnteroSinErroresScanner() {
         int num = 0;
         boolean repetir = true;
-
         do {
-            System.out.println("Introduce el numero entero");
-            try {
+            do {
+                System.out.println("Introduce el numero entero");
+                try {
 
-                num = teclado.nextInt();
-                repetir = false;
+                    num = teclado.nextInt();
+                    repetir = false;
 
-            } catch (InputMismatchException ime) {
-                System.out.println("No has introducido un numero entero");
-                //limpio buffer
-                teclado.nextLine();
-            }
+                } catch (InputMismatchException ime) {
+                    System.out.println("No has introducido un numero entero");
+                    //limpio buffer
+                    teclado.nextLine();
+                }
 
-        } while (repetir);
+            } while (repetir);
 
+        } while (num <= 0);
         return num;
     }
-    
-      public static double leerDecimalesSinErroresScanner() {
+
+    public static double leerDecimalesSinErroresScanner() {
         double num = 0.0;
         boolean repetir = true;
-
         do {
-            System.out.println("Introduce el numero decimal");
-            try {
+            do {
+                System.out.println("Introduce el numero decimal");
+                try {
 
-                num = teclado.nextDouble();
-                repetir = false;
+                    num = teclado.nextDouble();
+                    repetir = false;
 
-            } catch (InputMismatchException ime) {
-                System.out.println("No has introducido un numero decimal");
-                //limpio buffer
-                teclado.nextLine();
-            }
+                } catch (InputMismatchException ime) {
+                    System.out.println("No has introducido un numero decimal");
+                    //limpio buffer
+                    teclado.nextLine();
+                }
 
-        } while (repetir);
+            } while (repetir);
+        } while (num <= 0);
 
         return num;
     }
-    
-    
-    public static Persona pedirPersona(){
-    
-    Persona personaAux;
-    
+
+    public static Persona pedirPersona() {
+
+        Persona personaAux;
+
         System.out.println("¿Como se llama la persona?");
         String nombre = teclado.nextLine();
         System.out.println("¿Cuantos años tiene la persona?");
-        int edad=ServicioPersona.leerEnteroSinErroresScanner();
+        int edad = ServicioPersona.leerEnteroSinErroresScanner();
         System.out.println("¿Dinos el sexo. M si es mujer; H si es hombre, O si es otro?");
-        char sexo= teclado.next().charAt(0);
+        char sexo = teclado.next().charAt(0);
         System.out.println("¿Cuanto pesa la persona?");
-        double peso=ServicioPersona.leerDecimalesSinErroresScanner();
+        double peso = ServicioPersona.leerDecimalesSinErroresScanner();
         System.out.println("¿Cuanto mide la persona?");
         double altura = ServicioPersona.leerDecimalesSinErroresScanner();
-        
-        personaAux=new Persona(nombre, edad, sexo, peso, altura);
-    
-    
-    return personaAux;
-    
+
+        personaAux = new Persona(nombre, edad, sexo, peso, altura);
+
+        return personaAux;
+
     }
-    
-    
-    
-    
-    
-    
+
 }
